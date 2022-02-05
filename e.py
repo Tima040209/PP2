9e0a8fbef7e0a88b18da9706888c7253200a6eb0
Tima040209 <98690375+Tima040209@users.noreply.github.com>
2022-02-05 15:17:22 +0600

    Delete e.py

--- e.py
+++
@@ -1,9 +0,0 @@
-num = list(map(int,input().split()))
-k=0
-for i in range(2,num[0]):
-        if (num[0] % i == 0):
-            k+=1
-if (num[0]<=500) and (k==0) and (num[1]%2==0):
-        print("Good job!") 
-else :
-        print("Try next time!")          
