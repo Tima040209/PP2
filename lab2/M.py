c97f6b61a83565fe5baeb4d6c0a4e70868253318
Tima040209 <98690375+Tima040209@users.noreply.github.com>
2022-02-11 20:45:43 +0600

    Update M.py

--- lab2/M.py
+++ lab2/M.py
@@ -3,11 +3,11 @@ z = []
 while len(z) > 2:
     dat = input()
     dat = dat.split()
-    z.append((dat[0],dat[1],dat[2]))
+    z.append((dat[-1],dat[-2],dat[0]))
 z.reverse()
 z.sort()
 sec = []
 for i in z:
-    sec.append((i[2],i[1],i[0])) 
+    sec.append((i[-1],i[-2],i[0])) 
 for i in sec:
     print(i)       
