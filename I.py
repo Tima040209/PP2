cdc47c9060529be4059afcdbbdce98cb4607e904
Tima040209 <98690375+Tima040209@users.noreply.github.com>
2022-02-11 20:41:59 +0600

    Delete I.py

--- I.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-disks = []
-cnt = 0
-for i in range(n):
-    oper = input().split()
-    if oper[0]=="1": disks.append(oper[1])
-    else:cnt+=1
-for i in range(cnt): 
-    print(disks[i],end=' ')    
